344ee10e5719c38232dd751cdb06234e970311ef
rcatlovelua <fderdsvht@gmail.com>
2026-03-21 16:11:32 +0300

    Update IloveMoscowTime.js

--- api/IloveMoscowTime.js
+++ api/IloveMoscowTime.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
+  // РАЗРЕШАЕМ CORS: это позволит любому сайту делать запросы к твоему API
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const mskTime = new Intl.DateTimeFormat("ru-RU", {
     timeZone: "Europe/Moscow",
     hour: "2-digit",
