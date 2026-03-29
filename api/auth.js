34193f75149f62bccc986f62db83dcc9859231aa
rcatlovelua <fderdsvht@gmail.com>
2026-03-29 23:12:38 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,12 +5,14 @@ export default function handler(req, res) {
   }
 
   const { password } = req.body;
-
+  
   // Сравниваем пароль из запроса с переменной окружения
   if (password === process.env.ADMINPLUS_KEY) {
     return res.status(200).json({ 
       success: true, 
-      token: "SESSION_ACTIVE_XYZ" // В идеале тут должен быть JWT, но для начала хватит и этого
+      token: "SESSION_ACTIVE_XYZ", // В идеале тут должен быть JWT, но для начала хватит и этого
+      supatkn: process.env.SUPABASE_URL
+      supasrl: process.env.SUPABASE_SERVICE_ROLE_KEY
     });
   } else {
     return res.status(401).json({ 
