7237a56dbd446c7d1b78b68c1b05090b18a7bfb9
rcatlovelua <fderdsvht@gmail.com>
2026-08-17 11:29:52 +0300

    Update yandex.js

--- api/yandex.js
+++ api/yandex.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const clientId = process.env.YANDEX_CLIENT_ID;
-  const redirectUri = `${process.env.APP_URL || 'https://justrcat.lol'}/api/auth/callback`;
+  const redirectUri = `${process.env.APP_URL || 'https://justrcat.lol'}/api/callback`;
   
   const yandexAuthUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
   return res.redirect(yandexAuthUrl);
